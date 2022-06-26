c076bdf2ba27f28ff5e77f97daaecc33193c0c4d
HAYOON <dimple9347@gmail.com>
2022-06-26 22:33:33 +0900

    Portfolio first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+
+const navBtn = document.querySelector('.header_nav_btn');
+
+navBtn.addEventListener('click', function(){
+
+  const navMenu = document.querySelector('.header_nav_menu');
+  navMenu.classList.toggle('active');
+
+})
